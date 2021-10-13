4f5a2bea20b389a1f9d0f3d0320a03b0d69a3058
Daniel Grazziotti <dgplays14@gmail.com>
2021-10-13 01:44:25 -0300

    added animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 const switchBtn = document.querySelector('.js-switch')
+const elementsWithAnimation = document.querySelectorAll('[data-anime]')
+
+elementsWithAnimation.forEach( el => setTimeout( () => el.classList.add('animate', 1)))
 
 switchBtn.addEventListener('click', () => document.body.classList.toggle('dark--mode'))
